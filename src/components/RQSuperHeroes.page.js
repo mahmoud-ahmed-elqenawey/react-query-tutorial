3a848843caff70d067dc907c7e01801ac9c97be2
QeNo <moodiiy.98@gmail.com>
2022-09-19 22:43:09 +0200

    fetch data

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,23 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
 export const RQSuperHeroesPage = () => {
-    return <h2>React Query Super Heroes Page</h2>;
+    const fetchSuperHeroes = () => {
+        return axios.get("http://localhost:3001/superheroes");
+    };
+    const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+    console.log(data, "data");
+
+    if (isLoading) {
+        return <h2>Loading...</h2>;
+    }
+    return (
+        <>
+            <h2>React Query Super Heroes Page</h2>
+            {data?.data.map((hero) => (
+                <div key={hero.name}>{hero.name}</div>
+            ))}
+        </>
+    );
 };
